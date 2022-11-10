d239f683040dd416fc4f28caf1b3e900607af090
vamshikrishna26 <117000942+vamshikrishna26@users.noreply.github.com>
2022-11-10 12:13:39 +0530

    Add files via upload

---
+++ Label, Geometry, Maxsize & Minsize.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+vamshikrishna_root=Tk()
+#width x height
+vamshikrishna_root.geometry("888x222")
+#width , height
+vamshikrishna_root.minsize(200,100)
+
+#width , height
+vamshikrishna_root.maxsize(700,500)
+
+ivar= Label(text="i am vamshi krishna and this is my first GUI label")
+
+ivar.pack()
+
+vamshikrishna_root.mainloop()
